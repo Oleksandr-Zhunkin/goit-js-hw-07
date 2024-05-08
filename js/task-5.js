7a539a1949b7d70b1d3b3a2fd5f4a015a4b94a68
Oleksandr_Zhynkin <godinsighted@gmail.com>
2024-05-09 01:41:19 +0300

    6 task

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const spanColor = document.querySelector('.color');
+
+changeColor.addEventListener('click', e => {
+  const newColor = (bodyEl.style.backgroundColor = getRandomHexColor());
+  spanColor.style.color = newColor;
+});
